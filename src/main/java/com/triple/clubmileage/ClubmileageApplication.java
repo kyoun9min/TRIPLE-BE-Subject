e7da1baf17525384d0f5df074c86251d25c4f192
kyoun9min <rtong77@naver.com>
2022-06-23 00:57:53 +0900

    first commit

---
+++ src/main/java/com/triple/clubmileage/ClubmileageApplication.java
@@ -0,0 +1,13 @@
+package com.triple.clubmileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubmileageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubmileageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/triple/clubmileage/ClubmileageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triple.clubmileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubmileageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
