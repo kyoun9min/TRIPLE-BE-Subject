fd31d3d2d576092776d6b327bfe86a002099b9ca
kyoun9min <rtong77@naver.com>
2022-06-24 22:35:32 +0900

    feat : 포인트 조회 기능 구현

--- src/main/java/com/triple/clubmileage/controller/PointController.java
+++ src/main/java/com/triple/clubmileage/controller/PointController.java
@@ -1,12 +1,28 @@
 package com.triple.clubmileage.controller;
 
+import com.triple.clubmileage.dto.PointDTO;
+import com.triple.clubmileage.entity.User;
 import com.triple.clubmileage.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class PointController {
 
+    private final UserRepository userRepository;
+
+    @GetMapping("/point/{userId}")
+    public ResponseEntity getPoint(@PathVariable String userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new HttpClientErrorException(HttpStatus.BAD_REQUEST));
+        return ResponseEntity.status(HttpStatus.OK).body(new PointDTO(user.getPoint()));
+    }
 }
---
+++ src/main/java/com/triple/clubmileage/dto/PointDTO.java
@@ -0,0 +1,15 @@
+package com.triple.clubmileage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class PointDTO {
+
+    private int point;
+}
