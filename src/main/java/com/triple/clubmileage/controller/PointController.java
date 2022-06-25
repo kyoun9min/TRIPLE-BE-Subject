2d67f01f9912bc6e2287c223af4c25777e55282d
kyoun9min <rtong77@naver.com>
2022-06-25 15:18:04 +0900

    refactor : UserNotFound 예외처리 빼먹은 부분 수정

--- src/main/java/com/triple/clubmileage/controller/PointController.java
+++ src/main/java/com/triple/clubmileage/controller/PointController.java
@@ -2,6 +2,7 @@ package com.triple.clubmileage.controller;
 
 import com.triple.clubmileage.dto.PointDTO;
 import com.triple.clubmileage.entity.User;
+import com.triple.clubmileage.exception.UserNotFoundException;
 import com.triple.clubmileage.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
 
 
 @RestController
@@ -21,7 +21,7 @@ public class PointController {
     @GetMapping("/point/{userId}")
     public ResponseEntity getPoint(@PathVariable String userId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new HttpClientErrorException(HttpStatus.BAD_REQUEST));
+                .orElseThrow(() -> new UserNotFoundException("해당 유저가 존재하지 않습니다."));
         return ResponseEntity.status(HttpStatus.OK).body(new PointDTO(user.getPoint()));
     }
 }
